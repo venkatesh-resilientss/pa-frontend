59a34816d01671bd8aa6b1845c90e2ac067ad6ae
Vamsi Mathala <vamsi.mathala@soulpageit.com>
2023-12-19 10:51:39 +0530

    budgets fixes

--- components/MyProject/Configurations/Budget/index.tsx
+++ components/MyProject/Configurations/Budget/index.tsx
@@ -1,15 +1,8 @@
 import AllBudgetTable from "./AllBudgetTable";
-import BudgetBulkUploadPopup from "./BudgetBulkUploadPopup";
-import DeleteBudgetPopup from "./DeleteBudgetPopup";
-import { useState } from "react";
 function Budgets() {
-  const [searchText, setSearchText] = useState("");
-  const [rerender, setRerender] = useState(false);
   return (
     <div>
-      <BudgetBulkUploadPopup {...{rerender,setRerender}} />
-      <DeleteBudgetPopup />
-      <AllBudgetTable {...{ rerender, searchText, setSearchText }} />
+      <AllBudgetTable  />
     </div>
   );
 }
